4c51898a1da7de3e9d9afa0e13596fec6b89eec5
Quannguyen830 <nguyenvietquan512@gmail.com>
2025-11-01 12:21:35 +0700

    init

---
+++ src/main/java/com/example/kafkaclone/KafkaCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaCloneApplication.class, args);
+    }
+
+}
